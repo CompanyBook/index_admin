a25180c967b884e8a9b6c365b56d2595b0a57e2a
Rune Lind <rulind@gmail.com>
2012-01-31 17:19:38 +0100

    more helper methodes

--- app/helpers/servers_helper.rb
+++ app/helpers/servers_helper.rb
@@ -2,4 +2,17 @@ module ServersHelper
   def header_for(file_info)
     file_info.size + (file_info.avail_space.present? ? '/' + file_info.avail_space : '')
   end
+
+  def is_solr_index?(file_info)
+    file_info.children.find { |dir| dir.name == 'conf' }
+  end
+
+  def is_below_solr?(file_info)
+    found = file_info
+    while(found.parent)
+      return true if found.parent.name == 'solr'
+      found = found.parent
+    end
+    false
+  end
 end
