a10a13962fa9320f6ed6095c7941936e65e6c52e
Rune Lind <rulind@gmail.com>
2012-02-02 17:20:54 +0100

    added run_solr_index_copy_and_merge

--- app/models/merge_job.rb
+++ app/models/merge_job.rb
@@ -1,2 +1,7 @@
+require 'remote_server'
+
 class MergeJob < ActiveRecord::Base
+  def run_solr_index_copy_and_merge(hdfs_src_path, server_dest_path)
+    RemoteServer.new(dest_server).run_solr_index_copy_and_merge(hdfs_src_path, server_dest_path)
+  end
 end
