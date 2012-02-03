bb31bff3baa70645bce581569d60afe77be035ae
Rune Lind <rulind@gmail.com>
2012-02-03 17:28:52 +0100

    Added run_solr_index_copy_and_merg call to remote server

--- app/models/merge_job.rb
+++ app/models/merge_job.rb
@@ -1,7 +1,21 @@
 require 'remote_server'
 
 class MergeJob < ActiveRecord::Base
-  def run_solr_index_copy_and_merge(hdfs_src_path, server_dest_path)
-    RemoteServer.new(dest_server).run_solr_index_copy_and_merge(hdfs_src_path, server_dest_path)
+  def is_running
+    false
+  end
+
+  def output
+    RemoteServer.new(dest_server,'Rune','~/Source/hdfs_copy_solr_index').log_output
+  end
+
+  def run_solr_index_copy_and_merg
+    args = { simulate: true,
+             hadoop_src: hdfs_src,
+             copy_dst: dest_path,
+             max_merge_size: '150G',
+             dst_distribution: [copy_dst]  }
+
+    RemoteServer.new(dest_server,'Rune','~/Source/hdfs_copy_solr_index').run_solr_index_copy_and_merge(args)
   end
 end
