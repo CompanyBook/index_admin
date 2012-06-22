ae51fdbc2fc847a430a2d98a9edf1ab725c39862
Rune Lind <rulind@gmail.com>
2012-06-22 13:26:21 +0200

    handle solr server not responding to url request

--- lib/solr_core_admin.rb
+++ lib/solr_core_admin.rb
@@ -2,22 +2,41 @@ require 'open-uri'
 require 'json'
 
 class SolrCoreAdmin
-  attr_reader :name, :port
+  attr_reader :name, :port, :id
+
   @@cmd_cache = {}
 
-  def initialize(name, port)
+  def initialize(name, port, id)
     @name = name
     @port = port
+    @id = id
     @url = "http://#{name}:#{port}/solr/admin/cores?action=STATUS&wt=json"
+
   end
 
   def servers
-    result = @@cmd_cache[@url] ||= open("#{@url}").read
-    map = JSON.parse(result)
-    map['status'].values
+    begin
+      result = @@cmd_cache[@url] ||= open("#{@url}").read
+      map = JSON.parse(result)
+      @is_ok = true
+      return map['status'].values
+    rescue Exception => e
+      @is_ok = false
+      puts "#{@url} have issues and don't return servers \n\n#{e.message}"
+    end
+  end
+
+  def is_ok
+    return @is_ok if @is_ok!=nil
+    servers
+    @is_ok
   end
 
   def server_names
     servers.collect { |item| item['name'] }
+  end
+
+  def to_s()
+    @url
   end
 end
